ac23251c6b90df657ff63123f888758f4bfc7a4e
TechDev-CEO <shoriful521399@gmail.com>
2022-01-22 17:03:15 +0600

    Path Complete

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Home from './component/Home/Home';
 import About from './component/About/About';
 import NotFound from './component/NotFound/NotFound';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Friend from './component/Friends/Friend';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <About></About>
-      <NotFound></NotFound>
+     <BrowserRouter>
+     <Routes>
+       <Route path="home" element={<Home/>}/>
+       <Route path="About" element={<About/>}/>
+       <Route path="friends" element={<Friend></Friend>}/>
+       <Route exact path="/" element={<Home></Home>}/>
+       <Route element={<NotFound></NotFound>}/>
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
