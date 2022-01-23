8157a028393730d0865c4852abaf6b2a7284c020
TechDev-CEO <shoriful521399@gmail.com>
2022-01-23 18:13:00 +0600

    Button use korchi sekhanne Navigate use kore kaj korchi

--- src/component/Friend/Friend.js
+++ src/component/Friend/Friend.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Friend = (props) => {
     const {id, name, email,address,website,phone}=props.friend;
+    const navigate  = useNavigate();
+   
     const friendStyle={
         fontweigt:'bold',
         padding:'10px',
@@ -10,12 +12,18 @@ const Friend = (props) => {
         borderRadius:'10px',
     }
     const url=`/friend/${id}`;
+    const handleFriendClick = () =>{
+        navigate(`/friend/${id}`)
+    }
     return (
         <div style={friendStyle}>
             <h1>Name:{name}</h1>
             <h2>Email:{email}</h2>
             <p>Adress: {address.street}</p>
             <Link to={url}>Visit me</Link>
+            <Link to={url}> <button>Visit Me</button></Link>
+            
+            <button onClick={handleFriendClick}>Visit Me 2</button>
             
         </div>
     );
